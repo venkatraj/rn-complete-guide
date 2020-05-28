bce7a90f3c7344e6855b0e0df4f5eabda3e386c7
Venkat <venkat@codinggeek.com>
2020-05-28 10:18:38 +0530

    flexbox deep dive

--- App.js
+++ App.js
@@ -3,27 +3,66 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50, width: '30%' }}>
+    <View
+      style={{ padding: 50, height: 400, width: '30%', flexDirection: 'row' }}
+    >
       <View
         style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            borderColor: 'black',
-            borderWidth: 1,
-            padding: 5,
-            margin: 10,
-            flexGrow: 1,
-          }}
-        />
-        <Button title="ADD" />
+        <Text>1</Text>
       </View>
-      <View></View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
+
+// export default function App() {
+//   return (
+//     <View style={{ padding: 50, width: '30%' }}>
+//       <View
+//         style={{
+//           flexDirection: 'row',
+//           justifyContent: 'space-between',
+//           alignItems: 'center',
+//         }}
+//       >
+//         <TextInput
+//           placeholder="Course Goal"
+//           style={{
+//             borderColor: 'black',
+//             borderWidth: 1,
+//             padding: 5,
+//             margin: 10,
+//             flexGrow: 1,
+//           }}
+//         />
+//         <Button title="ADD" />
+//       </View>
+//       <View></View>
+//     </View>
+//   );
+// }
